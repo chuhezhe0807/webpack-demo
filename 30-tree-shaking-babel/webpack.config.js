f7ead695b4630d56ce0ef3b4ed2f16020c33f87d
chuhezhe <chuhezhe0807@163.com>
2025-04-27 22:37:33 +0800

    learning: Tree Shaking; · Tree Shaking是基于ESM来的，所以保证Tree Shaking时代码没有被转换为Commonjs等非ESM代码;

--- 30-tree-shaking-babel/webpack.config.js
+++ 30-tree-shaking-babel/webpack.config.js
@@ -14,8 +14,9 @@ module.exports = {
             presets: [
               // 如果 Babel 加载模块时已经转换了 ESM，则会导致 Tree Shaking 失效
               // ['@babel/preset-env', { modules: 'commonjs' }]
-              // ['@babel/preset-env', { modules: false }]
-              // 也可以使用默认配置，也就是 auto，这样 babel-loader 会自动关闭 ESM 转换
+              // ['@babel/preset-env', { modules: 'auto' }] babel会根据目标环境自动决定是否需要将ES6模块转换为其他模块格式
+              // 如果目标环境支持原生ESM，babel不会转化模块格式，保留原生ESM，就可以支持Tree Shaking
+              // 不支持原生ESM，则会将ESM转换为Commonjs
               ['@babel/preset-env', { modules: 'auto' }]
             ]
           }
@@ -24,7 +25,7 @@ module.exports = {
     ]
   },
   optimization: {
-    // 模块只导出被使用的成员
+    // 模块只导出被使用的成员(Tree Shaking)
     usedExports: true,
     // 尽可能合并每一个模块到一个函数中
     // concatenateModules: true,
