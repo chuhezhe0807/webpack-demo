ceab778ea5ef2d71c6cdce0963ba1ce5fa90ee69
chuhezhe <chuhezhe0807@163.com>
2025-04-27 22:30:42 +0800

    learning: sideEffects的使用;

--- 31-side-effects/webpack.config.js
+++ 31-side-effects/webpack.config.js
@@ -16,6 +16,9 @@ module.exports = {
     ]
   },
   optimization: {
+    // sideEffects 需要在optimization中显示开启并在项目的package.json中配置sideEffects才能生效
+    // package.json中的sideEffects: true表示所有模块都有副作用，false表示所有模块没有副作用
+    // 如果明确知道某几个模块是有副作用的，可以声明package.json为有副作用的文件组成的数组，如：sideEffects: ['src/module1.js', 'src/module2.js']
     sideEffects: true,
     // 模块只导出被使用的成员
     // usedExports: true,
